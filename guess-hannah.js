de5eefdd5561000673bc1be1446d75f2839eda8c
hsteadman <54716362+hsteadman@users.noreply.github.com>
2019-10-24 09:59:15 -0400

    Update guess-hannah.js

--- guess-hannah.js
+++ guess-hannah.js
@@ -1,6 +1,4 @@
 /* guess merit badge by hannah */
-// guessing game code just like you practiced girl you got this
-// remember: variables. prompt the guesser for the answer and alert them. while, if, else if, else. first one too low, second one too high, q to quit as a way to get out of the loop quick, and last one got it. dont forget to keep track of the answer somewhere, and finish every line with a "bow" ;) lets do this :D
 
 var answer=Math.floor(Math.random()*100)+1;
 var guess=0;
