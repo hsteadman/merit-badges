c8f56e5edf95da1a46e3df4bb5a167c41df17bab
hsteadman <54716362+hsteadman@users.noreply.github.com>
2019-10-25 10:01:40 -0400

    Add files via upload

---
+++ guess-advanced-functions-hannah.js
@@ -0,0 +1,62 @@
+/* guess advanced functions merit badge by hannah */
+
+var games=0;
+var again=true;
+var totalTurns=0;
+while(again==true){
+	games++;
+	var guess=0;
+	var turns=0;
+	var answer=Math.floor(Math.random()*100)+1;
+	console.log(answer);
+	while(guess!=answer){
+		guess=prompt("Guess my number between 1-100! Press Q to quit!");
+		turns++;
+		if(guess=="q"){
+			alert("Goodbye!");
+			break;
+		}
+		if(validate(guess)==true){
+			if(guess<answer){
+				alert("Too low! Go higher!");
+			}
+			else if(guess>answer){
+				alert("Too high! Go lower!");
+			}
+			else if(guess==answer){
+				gameStats(turns);
+				again=newGame();
+			}
+		}
+	}
+	if(guess=="q"){
+		break;
+	}
+}
+
+function validate(guess){
+	if(guess>0&&guess<101){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+function gameStats(turns,totalTurns){
+	alert("You guessed it in "+turns+" turns! Great job!");
+	totalTurns+=turns;
+	var averageTurns=totalTurns/games;
+	alert("You won "+games+" games with an average of "+averageTurns+" turns per game! Great job!");
+}
+
+function newGame(again){
+	again=prompt("Play again? (y/n)");
+	if(again=="y"){
+		return true;
+	}
+	else{
+		alert("Thanks for playing!");
+		return false;
+	}
+}
