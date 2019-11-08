602df25cce71f01a956aafbaf0f8ac5327513d73
hsteadman <54716362+hsteadman@users.noreply.github.com>
2019-11-08 10:07:53 -0500

    Update nim-simple-hannah.js

--- nim-simple-hannah.js
+++ nim-simple-hannah.js
@@ -37,6 +37,7 @@ while(again==true){
 alert("Thanks for playing Nim with me!");
 
 /* functions */
+
 function CPUTurn(){
 	turn=Math.floor(Math.random()*3)+1;
 	alert("CPU counts "+turn);
@@ -48,53 +49,6 @@ function userTurn(){
 	if(turn>0&&turn<4){
 		alert("You count "+turn);
 		return turn;
-	}
-	else{
-		alert("Guess is out of range, try again.");
-	}
-}
-
-function nextSwitch(next){
-	if(next==0){
-		next=1;
-		return next;
-	}
-	else{
-		next=0;
-		return next;
-	}
-}
-
-function playAgain(again){
-	again=prompt("Play again? y/n");
-	if(again=="y"){
-		return true;
-	}
-	else{
-		return false;
-	}
-}
-
-function declareWinner(){
-	if(next==0){
-		alert("You lose!");
-	}
-	else{
-		alert("You win!");
-	}
-}
-
-function CPUTurn(turn){
-	turn=Math.floor(Math.random()*3)+1;
-	alert("CPU counts "+turn);
-	return turn;
-}
-
-function userTurn(turn){
-	turn=prompt("Pick a number 1-3");
-	if(turn>0&&turn<4){
-		alert("You count "+turn);
-		return Number(turn);
 	}
 	else{
 		alert("Guess is out of range, try again");
@@ -121,7 +75,7 @@ function declareWinner(){
 	}
 }
 
-function playAgain(){
+function playAgain(again){
 	again=prompt("Play again? (y/n)"){
 		if(again=="y"){
 			return true;
