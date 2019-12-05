c2c5f03caf37d2d86f73e2e05df696aa6038db81
hsteadman <54716362+hsteadman@users.noreply.github.com>
2019-12-05 11:33:51 -0500

    Create nim-trainer-hannah.js

---
+++ nim/nim-trainer-hannah.js
@@ -0,0 +1,95 @@
+/* nim simple merit badge by hannah */
+/* globals */
+var games=0;
+var again=true;
+var next=0;
+var count=0;
+var turn=0;
+/* main */
+while(again==true){
+	next=games%2;
+	games++;
+	alert("Game "+games+" start!");
+	count=0;
+	while(count<21){
+		if(next==0){
+			turn=CPUTurn();
+			count+=turn;
+			alert("The count is now "+count+".");
+			next=nextSwitch(next);
+		}
+		else{
+			turn=userTurn();
+			count+=turn;
+			alert("The count is now "+count+".");
+			next=nextSwitch(next);
+		}
+	}
+	declareWinner();
+	alert("Games played: "+games);
+	if(playAgain()==false){
+		break;
+	}
+}
+alert("Thanks for playing Nim with me!");
+
+/* functions */
+function CPUTurn(){
+	let turn=0;
+	if(count==19||count==20){
+		turn=1;
+	}
+	else if(count==18){
+		turn=Math.floor(Math.random()*2)+1;
+	}
+	else{
+		turn=Math.floor(Math.random()*3)+1;
+	}
+	alert("CPU Counts "+turn+".");
+	return turn;
+	}
+
+function userTurn(){
+	var goodTurn=false;
+	while(goodTurn==false){
+		turn=prompt("Pick a number 1-3");
+		if(turn>0&&turn<4&&Math.floor(turn)==turn){
+			goodTurn=true;
+			alert("You count "+turn+".");
+		}
+		else{
+			alert("Number is out of range, try again.");
+		}
+	}
+	return Number(turn);
+}
+
+function nextSwitch(next){
+	if(next==0){
+		next=1;
+		return next;
+	}
+	else{
+		next=0;
+		return next;
+	}
+}
+
+function declareWinner(){
+	if(next==0){
+		alert("You lose!");
+	}
+	else{
+		alert("You win!");
+	}
+}
+
+function playAgain(again){
+	again=prompt("Play again? (y/n)");
+	if(again=="y"){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
