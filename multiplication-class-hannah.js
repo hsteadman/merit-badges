3a5e78322c4f463396279bb244c00b01359ff313
hsteadman <54716362+hsteadman@users.noreply.github.com>
2020-01-15 10:56:08 -0500

    Update multiplication-class-hannah.js

--- multiplication-class-hannah.js
+++ multiplication-class-hannah.js
@@ -7,17 +7,6 @@
  * stop: boolean - start at false
  * again: boolean - start at true
  */ 
-//var tRange=0;
-//var bRange=0;
-var wrong=0;
-var stop=false;
-var again=true;
-var questions=setup();
-var question=0;
-var x=Math.floor(Math.random()*6)+3;
-var y=Math.floor(Math.random()*6)+3;
-var equation=prompt(x+" + "+y+" = ?");
-var solution=x*y;
 
 /* main code 
  * use a while loop to keep calling main function while again = true. 
@@ -39,71 +28,95 @@ alert("Thanks for playing!");
  * @param none
  * @return again (boolean)
  */ 
- 
+
 function main(){
+	var wrong=0; stop=false; again=true; question=0; x=Math.floor(Math.random()*6)+3; y=Math.floor(Math.random()*6)+3; equation=prompt(x+" + "+y+" = ?"); solution=x*y; tRange=0; bRange=0; wrongAnswers=0;
+	var questions=setup();
 	for(let question=questioner();question<=questions;question++){
-		questioner();
-		userInput();
+		wrong+=questioner();
 	}
-	alert("You did it! You got "+wrong+" out of "+questions);
-	again=confirm("Do this again?");
-}
+	endQuiz(again);
+	
+	/* function setup
+	  * set bottomRange
+	  * set topRange
+	  * set questions (how many?) 
+	  * @param: none
+	  * @return: questions
+	  */
+	function setup(){
+		 questions=parseInt(prompt("How many questions?"));
+		 bRange=parseInt(prompt("Lowest factor?"));
+		 tRange=parseInt(prompt("Highest factor?"));
+		 alert("Enter 'stop' to quit the program.");
+		console.log("setup");
+		 return questions;
+	}
 
-/* function setup
- * set bottomRange
- * set topRange
- * set questions (how many?) 
- * @param: none
- * @return: questions
- */
- 
- function setup(){
-	 questions=parseInt(prompt("How many questions?"));
-	// tRange=parseInt(prompt("Highest factor?"));
-	// bRange=parseInt(prompt("Lowest factor?"));
-	alert("awawa");
-	 return questions;
- }
- 
-/* function questioner 
- * generate X and Y factors
- * concatenate X & Y for equation (a string prompt)
- * calculate solution
- * call userInput, sending equation and solution
- * userInput returns # wrong (0 or 1) so take this and return it to main function
- * @param none
- * @return wrong
- */
- 
-function questioner(){
-	userInput();
-	alert("wawawa");
-	return wrong;
-}
+	/* function questioner 
+	  * generate X and Y factors
+	  * concatenate X & Y for equation (a string prompt)
+	  * calculate solution
+	  * call userInput, sending equation and solution
+	  * userInput returns # wrong (0 or 1) so take this and return it to main function
+	  * @param none
+	  * @return wrong
+	  */
+	function questioner(){
+		let wrong=0;
+		x=Math.floor(Math.random()*((tRange+1)-bRange))+bRange;
+		y=Math.floor(Math.random()*((tRange+1)-bRange))+bRange;
+		let equation=(x+" + "+y+" = ?");
+		let solution=x*y;
+		wrong=userInput(equation,solution);
+		console.trace("Question "+question+": "+x+" * "+y+" with "+wrong+ "wrong.");
+		return wrong;
+	}
 
-/* function userInput
- * prompt the equation to the user, take input
- * if input equals "stop" set stop as true, otherwise
- * check if input equals solution (say "correct!" and return wrong)
- * Add 1 to wrong if incorrect and say "incorrect. try again. enter 'stop' to quit trying"
- * keep asking until solution entered
- * return 0 if 0 wrong, 1 if 1 or more wrong.
- * @param none
- * @return wrong (integer, 0 or 1)
- */ 
- 
-function userInput(equation,solution,answer,x,y){
-	answer=prompt(equation);
-	if(answer=="stop"){
-		again=false;
+	/* function userInput
+	  * prompt the equation to the user, take input
+	  * if input equals "stop" set stop as true, otherwise
+	  * check if input equals solution (say "correct!" and return wrong)
+	  * Add 1 to wrong if incorrect and say "incorrect. try again. enter 'stop' to quit trying"
+	  * keep asking until solution entered
+	  * return 0 if 0 wrong, 1 if 1 or more wrong.
+	  * @param none
+	  * @return wrong (integer, 0 or 1)
+	  */ 
+	function userInput(equation,solution){
+		stop=false;
+		let wrong=0;
+		let input=0;
+		while(input!=solution&&stop==false){
+			input=prompt(equation);
+			if(input=="stop"){
+				stop=true;
+				questions=question;
+				return wrong;
+			}
+			else if(input==solution){
+				alert("Correct!");
+			}
+			else{
+				alert("Incorrect. Try again!");
+				wrong=1;
+			}
+		}
+		return wrong;
 	}
-	else if(answer==solution){
-		alert("Correct!");
+
+	function endQuiz(wrongAnswers){
+		if(wrongAnswers>0||stop==true){
+			again=confirm("You had "+wrongAnswers+" wrong out of "+questions+". \n Do this some more?");
+		}
+		else{
+			again=confirm("You did it! Try higher factors and try this some more?");
+		}
+		if(again==true){
+			main();
+		}
+		else{
+			alert("Hope you learned your times tables!");
+		}
 	}
-	else{
-		alert("Incorrect!");
-		wrong++;
-	}
-	alert("ewaeweae");
-	return wrong;
 }
